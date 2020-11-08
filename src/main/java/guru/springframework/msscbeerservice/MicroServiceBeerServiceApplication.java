f783c253e01b534918caa38981f4c80afa77fe8f
jasonc-dev <jasoncohen07@gmail.com>
2020-11-08 15:47:36 +0000

    Initial commit

---
+++ src/main/java/guru/springframework/msscbeerservice/MicroServiceBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.msscbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceBeerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceBeerServiceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeetDto.java
@@ -0,0 +1,34 @@
+package guru.springframework.msscbeerservice.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BeetDto {
+    private UUID id;
+    private Integer version;
+
+    private OffsetDateTime createdDate;
+    private OffsetDateTime lastModifiedDate;
+
+    private String beetName;
+
+    private BeerStyleEnum beerStyle;
+
+    private Long upc;
+
+    private BigDecimal price;
+
+    private Integer quantityOnHand;
+
+}
