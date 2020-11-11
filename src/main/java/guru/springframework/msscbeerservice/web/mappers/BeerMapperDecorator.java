afa750fb1f81b1dafc63f9e194fac923004c32ff
jasonc-dev <jasoncohen07@gmail.com>
2020-11-11 20:49:02 +0000

    Initial Commit

---
+++ src/main/java/guru/springframework/msscbeerservice/web/mappers/BeerMapperDecorator.java
@@ -0,0 +1,39 @@
+package guru.springframework.msscbeerservice.web.mappers;
+
+import guru.springframework.msscbeerservice.domain.Beer;
+import guru.springframework.msscbeerservice.services.inventory.BeerInventoryService;
+import guru.springframework.msscbeerservice.web.model.BeerDto;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public abstract class BeerMapperDecorator implements BeerMapper {
+
+    private BeerInventoryService beerInventoryService;
+    private BeerMapper mapper;
+
+    @Autowired
+    public void setBeerInventoryService(BeerInventoryService beerInventoryService) {
+        this.beerInventoryService = beerInventoryService;
+    }
+
+    @Autowired
+    public void setMapper(BeerMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    @Override
+    public BeerDto beerToToBeerDto(Beer beer) {
+        return mapper.beerToToBeerDto(beer);
+    }
+
+    @Override
+    public BeerDto beerToToBeerDtoWithInventory(Beer beer) {
+        BeerDto dto = mapper.beerToToBeerDto(beer);
+        dto.setQuantityOnHand(beerInventoryService.getOnHandInventory(beer.getId()));
+        return dto;
+    }
+
+    @Override
+    public Beer beerDtoToBeer(BeerDto beerDto) {
+        return mapper.beerDtoToBeer(beerDto);
+    }
+}
---
+++ src/test/java/guru/springframework/msscbeerservice/services/inventory/BeerInventoryServiceRestTemplateImplTest.java
@@ -0,0 +1,25 @@
+package guru.springframework.msscbeerservice.services.inventory;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Disabled
+@SpringBootTest
+class BeerInventoryServiceRestTemplateImplTest {
+
+    @Autowired BeerInventoryService beerInventoryService;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getOnHandInventory() {
+//        Integer qoh = beerInventoryService.getOnHandInventory(BeerLoader.BEER_1_UPC);
+//
+//        System.out.println(qoh);
+}
+}
